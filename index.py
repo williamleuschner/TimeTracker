c29b164e9ddbf3b8e0391d341d1b56d065d26017
William Leuschner <wel2138@rit.edu>
2017-03-12 09:21:51 -0400

    Add hidden input for name of subject

--- index.py
+++ index.py
@@ -8,36 +8,42 @@ app = Flask(__name__)
 def index():
     subjects = [
         {
+            'name': 'Introduction to Computer Science Theory',
             'shortname': 'CS Theory',
             'link': '#',
             'color': 'blue',
             'icon': 'CS1.svg'
         },
         {
+            'name': 'The Mechanics of Programming',
             'shortname': 'MoPs',
             'link': '#',
             'color': 'blue',
             'icon': 'CS2.svg'
         },
         {
+            'name': 'Modern Philosophy',
             'shortname': 'Philosophy',
             'link': '#',
             'color': 'green',
             'icon': 'Philosophy.svg'
         },
         {
+            'name': 'Probability and Statistics I',
             'shortname': 'Prob & Stat',
             'link': '#',
             'color': 'green',
             'icon': 'Math.svg'
         },
         {
+            'name': 'University Physics II',
             'shortname': 'UP II',
             'link': '#',
             'color': 'red',
             'icon': 'Physics.svg'
         },
         {
+            'name': 'Functional Yoga',
             'shortname': 'Yoga',
             'link': '#',
             'color': 'purple',
